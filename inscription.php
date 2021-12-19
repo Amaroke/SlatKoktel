9ed748f590f4494c620d92432c5c51dae791ee43
Guillaume <guillaume.zimol@icloud.com>
2021-12-19 21:25:34 +0100

    inscription + retour menu (mettre plus de filtre)

--- inscription.php
+++ inscription.php
@@ -1,6 +1,6 @@
 <?php
 
-echo 'INITIALISATION :<br/>';
+//echo 'INITIALISATION :<br/>';
 /*
 * Ici on récupère les informations du form de la page signup.html
 */
@@ -16,75 +16,95 @@ $uti_ville = htmlspecialchars($_POST["uti_ville"]);
 $uti_codePostal = htmlspecialchars($_POST["uti_codePostal"]);
 $uti_mdp = htmlspecialchars($_POST["uti_mdp"]);
 $uti_mdp2 = htmlspecialchars($_POST["uti_mdp2"]);
+$mtn = date("Y-m-d");
 
 /*
 echo 'VERIFICATION :<br/>';
 echo $uti_pseudo;
 echo "<br>".$uti_email;
 echo "<br>".$uti_prenom;
-echo $uti_mdp;
-echo $uti_mdp2;
-exit;
+echo "<br>".$uti_mdp;
+echo "<br>".$uti_mdp2;
+echo "<br>".$uti_age;
+echo "<br>".$uti_sexe;
+echo "<br>".$uti_telephone;
+echo "<br>".$uti_adresse;
+echo "<br>".$uti_ville;
+echo "<br>".$uti_codePostal;
+echo "<br>".$mtn;
+echo "<br>";
+echo "<br>";
 */
 if(strlen($uti_pseudo) > 0 && strlen($uti_email) > 0 && strlen($uti_mdp) > 0 && strlen($uti_mdp2) > 0 && $uti_mdp == $uti_mdp2){
     //on fait la connexion à la base de données 
     /* Envoi des requêtes dans la base de données */
-    echo 'TRY :<br/>';
+    //echo 'TRY :<br/>';
     try
     {
-        echo 'TENTATIVE DE CONNEXION BDD :<br/>';
-        $bdd = new PDO('mysql:host=localhost;dbname=SlatKoktel;charset=utf8;charset=utf8', 'root', 'root1');
+        //echo 'TENTATIVE DE CONNEXION BDD :<br/>';
+        $bdd = new PDO('mysql:host=localhost;dbname=SlatKoktel;charset=utf8;', 'root', 'root1');
         
-        echo 'LOGIN EXISTE :<br/>';
+        //echo 'LOGIN EXISTE :<br/>';
         //tester si le login existe ou non
-        $test_login = $bdd->prepare("SELECT uti_pseudo FROM Utilisateurs WHERE uti_pseudo = '".$login."' "); 
+        $test_login = $bdd->prepare("SELECT uti_pseudo FROM Utilisateurs WHERE uti_pseudo = '".$uti_pseudo."' "); 
         
         $test_login->execute(); //execute
         $nb_login = $test_login->rowCount(); //nombre de resultats
 
-        echo 'MAIL EXISTE :<br/>';
+        //echo 'nb_login :'.$nb_login.'<br/>';
+        //echo 'MAIL EXISTE :<br/>';
         //tester si le login existe ou non
         $test_mail = $bdd->prepare("SELECT uti_email FROM Utilisateurs WHERE uti_email = '".$uti_email."' "); 
         
         $test_mail->execute(); //execute
         $nb_mail = $test_mail->rowCount(); //nombre de resultats
         
-        echo 'INSERTION :<br/>';
+        //echo 'INSERTION :<br/>';
         /*
         echo $nb_login;
         echo $nb_mail; 
         */
         if($nb_login == 0 && $nb_mail == 0){
-            echo 'RENTRE DANS LE IF DE INSERTION :<br/>';
-            $insertionUser = 'INSERT INTO Utilisateurs(uti_pseudo, uti_mdp, uti_sexe, uti_prenom, uti_nom, uti_age, uti_email, uti_telephone, uti_adresse, uti_codePostal, uti_ville, uti_dateCreation) 
-            VALUES (:uti_pseudo, :uti_mdp, :uti_sexe, :uti_prenom, :uti_nom, :uti_age, :uti_email, :uti_tel, :uti_adresse, :uti_codePostal, :uti_ville, NOW());'; 
-            echo 'APRES REQUETE :<br/>';
-            $insertionUserRequete = $bdd->prepare($insertionUser);  //le programme s'arrete ici, je ne sais pas pq 
+            //echo 'RENTRE DANS LE IF DE INSERTION :<br/>';
+            // $insertionUser = 'INSERT INTO Utilisateurs(uti_pseudo) 
+            // VALUES(:uti_pseudo);'; 
+            $insertionUser = 'INSERT INTO Utilisateurs(uti_pseudo, uti_mdp, uti_sexe, uti_prenom, 
+            uti_nom, uti_age, uti_email, uti_telephone, uti_adresse, uti_codePostal, uti_ville, uti_dateCreation) 
+            VALUES (:uti_pseudo, :uti_mdp, :uti_sexe, :uti_prenom, 
+            :uti_nom, :uti_age, :uti_email, :uti_telephone, :uti_adresse, :uti_codePostal, :uti_ville, :mtn);'; 
+            //echo 'APRES REQUETE :<br/>';
+            $insertionUserRequete = $bdd->prepare($insertionUser);  
 
-            echo 'APRES EXECUTION REQUETES :<br/>';
+            //echo 'APRES EXECUTION:<br/>';
             //insère les données dans la bdd
-            $insertionData->execute([
-                'uti_pseudo'=> $uti_pseudo,
-                'uti_mdp'=>$uti_mdp,
-                'uti_email' =>$uti_email,
-                'uti_age' =>$uti_age,
-                'uti_prenom' =>$uti_prenom,
-                'uti_nom' =>$uti_nom,
-                'uti_telephone' =>$uti_telephone,
-                'uti_adresse' =>$uti_adresse,
-                'uti_codePostal' =>$uti_codePostal,
-                'uti_ville' =>$uti_ville,
-                'uti_dateCreation'=>NULL, //pour le moment 
-            ]);
 
-            echo 'APRES INSERTION DES DATAS :<br/>';
+            $stmt = $bdd->prepare($insertionUser); 
+            $stmt -> bindParam(':uti_pseudo',$uti_pseudo);
+            $stmt -> bindParam(':uti_mdp',$uti_mdp);
+            $stmt -> bindParam(':uti_sexe',$uti_sexe);
+            $stmt -> bindParam(':uti_prenom',$uti_prenom);
+            $stmt -> bindParam(':uti_nom',$uti_nom);
+            $stmt -> bindParam(':uti_age',$uti_age);
+            $stmt -> bindParam(':uti_email',$uti_email);
+            $stmt -> bindParam(':uti_telephone',$uti_telephone);
+            $stmt -> bindParam(':uti_adresse',$uti_adresse);
+            $stmt -> bindParam(':uti_codePostal',$uti_codePostal);
+            $stmt -> bindParam(':uti_ville',$uti_ville);
+            $stmt -> bindParam(':mtn',$mtn);
+            $stmt -> execute();
+
+            header('Location: index.html'); //reviens au menu
+
+
+
+
+            //echo 'APRES INSERTION DES DATAS :<br/>';
 
         }
         
         $test_login->closeCursor();
 
-        echo 'REDIRECTION :<br/>';
-        header('index.html'); //une fois insérer on revient à la page de départ
+        //echo 'REDIRECTION :<br/>';
     } catch(PDOException $e) {
         die('Erreur : ' . $e->getMessage());
     }     
